7303b83a9db6453198156f36cb5841b3d09ca78e
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-05 09:42:56 -0800

    commit tree 7b50d51182ad682e7b686a7a865d125a7d96ec04

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// Write your code in this file!
-const currentUser = 'Olivia Pomeroy';
+// // Write your code in this file!
+// const currentUser = 'Olivia Pomeroy';
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
 
+const currentUser = "Olivia Pomeroy";
+//const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+ const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+ const shortGreeting = `Welcome, ${currentUser.slice(0 ,1)}!`;
+
+console.log(shortGreeting);
